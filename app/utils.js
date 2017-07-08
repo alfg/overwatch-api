db1bcae6e3391742df4e09efe534d5ee388db9ce
Rerbun <Rerbun@users.noreply.github.com>
2017-07-09 01:06:47 +0200

    Update utils.js

--- app/utils.js
+++ app/utils.js
@@ -1,9 +1,14 @@
 const utils = {
     filterIncludes(include, data) {
-        if (!include || include.length == 0 || !data.hasOwnProperty(include[0])) {
+        // Filters response with include parameters.
+        if (!include || include.length == 0 || !data.hasOwnProperty(include[0]))
             return data;
-        }
         let first = include.shift();
+        if (typeof data[first] == "string"){
+            let output = {};
+            output[first] = data[first];
+            return output;
+        }
         return filterIncludes(include, data[first]);
     }
 }

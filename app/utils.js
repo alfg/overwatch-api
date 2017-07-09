43aeff49ad960747e1bef73975b5bcdf057434d7
Rerbun <Rerbun@users.noreply.github.com>
2017-07-09 23:47:35 +0200

    Update utils.js

--- app/utils.js
+++ app/utils.js
@@ -1,16 +1,11 @@
 const utils = {
     filterIncludes(include, data) {
         // Filters response with include parameters.
-        if (!include || include.length == 0 || !data.hasOwnProperty(include[0]))
+        if (!include || include.length == 0 || typeof data == "string" || !data.hasOwnProperty(include[0]))
             return data;
         let first = include.shift();
         let output = {};
-        if (typeof data[first] == "string"){
-            output[first] = data[first];
-        }
-        else{
-            output[first] = this.filterIncludes(include, data[first]);
-        }
+        output[first] = this.filterIncludes(include, data[first]);
         return output;
     }
 }

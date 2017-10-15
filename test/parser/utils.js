a081cb4d6809e975dd8e57020982dc336aae2cae
Jenko <kogajennifer@gmail.com>
2017-10-14 22:05:00 -0700

    added more tests

--- test/parser/utils.js
+++ test/parser/utils.js
@@ -3,7 +3,35 @@ import { getPrestigeLevel } from '../../app/parser/utils';
 
 
 test('get the prestige level 0', t => {
-    const code = '0x0250000000000918';
+    const code = "0x0250000000000918";
+    const level = getPrestigeLevel(code);
+
+    t.deepEqual(level, 0);
+});
+
+test('get the prestige level 1700', t => {
+    const code = "0x02500000000009CD";
+    const level = getPrestigeLevel(code);
+
+    t.deepEqual(level, 1700);
+});
+
+test('get the prestige level default if not exists', t => {
+    const code = "0x02500000";
+    const level = getPrestigeLevel(code);
+
+    t.deepEqual(level, 0);
+});
+
+test('get the prestige level default if not exists as integer', t => {
+    const code = 12345;
+    const level = getPrestigeLevel(code);
+
+    t.deepEqual(level, 0);
+});
+
+test('get the prestige level default if given a bad value', t => {
+    const code = "!@#!!";
     const level = getPrestigeLevel(code);
 
     t.deepEqual(level, 0);

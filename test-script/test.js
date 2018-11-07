e84ea72665af9c9aab243a26249dce6b20cd7255
Alf <alf.g.jr@gmail.com>
2018-11-06 19:39:28 -0800

    remove test-script.

--- test-script/test.js
+++
@@ -1,9 +0,0 @@
-const overwatch = require('overwatch-api');
-
-const platform = 'psn';
-const region = 'global';
-const tag = 'Drollestmaple03';
-
-overwatch.getProfile(platform, region, tag, (err, json) => {
-    console.log(json);
-});
